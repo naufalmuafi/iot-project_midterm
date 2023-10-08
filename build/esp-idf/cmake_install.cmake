e7d0e5fada38ddeddecf86aff39feca8f1f36903
Naufal Muafi <nmuafi1@gmail.com>
2023-10-08 16:18:30 +0700

    add: gitignore

--- build/esp-idf/cmake_install.cmake
+++ build/esp-idf/cmake_install.cmake
@@ -407,8 +407,3 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/esp/uts_iot-project/build/esp-idf/main/cmake_install.cmake")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("D:/esp/uts_iot-project/build/esp-idf/DHT22/cmake_install.cmake")
-endif()
-
