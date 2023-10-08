b099de6c33bece84c2f83ff938012f5a0d5b50f2
Naufal Muafi <nmuafi1@gmail.com>
2023-10-08 16:42:46 +0700

    add: DHT22 done

--- components/DHT22/DHT22.c
+++ components/DHT22/DHT22.c
@@ -1,24 +1,25 @@
-
 /*------------------------------------------------------------------------------
+
   DHT22 temperature & humidity sensor AM2302 (DHT22) driver for ESP32
+
   Jun 2017:	Ricardo Timmermann, new for DHT22
+
   Code Based on Adafruit Industries and Sam Johnston and Coffe & Beer. Please help
   to improve this code.
 
   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
   Unless required by applicable law or agreed to in writing, this
   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
   CONDITIONS OF ANY KIND, either express or implied.
+
   PLEASE KEEP THIS CODE IN LESS THAN 0XFF LINES. EACH LINE MAY CONTAIN ONE BUG !!!
+
 ---------------------------------------------------------------------------------*/
 
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 
-#include <stdio.h>
 #include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
 #include "driver/gpio.h"
 
 #include "DHT22.h"
@@ -27,7 +28,7 @@
 
 static const char *TAG = "DHT";
 
-int DHTgpio = 4; // my default DHT pin = 4
+int DHTgpio = GPIO_NUM_4; // my default DHT pin = 4
 float humidity = 0.;
 float temperature = 0.;
 
@@ -86,7 +87,7 @@ int getSignalLevel(int usTimeOut, bool state)
       return -1;
 
     ++uSec;
-    ets_delay_us(1); // uSec delay
+    esp_rom_delay_us(1); // uSec delay
   }
 
   return uSec;
@@ -95,7 +96,9 @@ int getSignalLevel(int usTimeOut, bool state)
 /*----------------------------------------------------------------------------
 ;
 ;	read DHT22 sensor
+
 copy/paste from AM2302/DHT22 Docu:
+
 DATA: Hum = 16 bits, Temp = 16 Bits, check-sum = 8 Bits
 
 Example: MCU has received 40 bits data from AM2302 as
@@ -104,22 +107,30 @@ Example: MCU has received 40 bits data from AM2302 as
 
 1) we convert 16 bits RH data from binary system to decimal system, 0000 0010 1000 1100 → 652
 Binary system Decimal system: RH=652/10=65.2%RH
+
 2) we convert 16 bits T data from binary system to decimal system, 0000 0001 0101 1111 → 351
 Binary system Decimal system: T=351/10=35.1°C
+
 When highest bit of temperature is 1, it means the temperature is below 0 degree Celsius.
 Example: 1000 0000 0110 0101, T= minus 10.1°C: 16 bits T data
+
 3) Check Sum=0000 0010+1000 1100+0000 0001+0101 1111=1110 1110 Check-sum=the last 8 bits of Sum=11101110
+
 Signal & Timings:
-  The interval of whole process must be beyond 2 seconds.
-  To request data from DHT:
-  1) Sent low pulse for > 1~10 ms (MILI SEC)
-  2) Sent high pulse for > 20~40 us (Micros).
-  3) When DHT detects the start signal, it will pull low the bus 80us as response signal,
-    then the DHT pulls up 80us for preparation to send data.
-  4) When DHT is sending data to MCU, every bit's transmission begin with low-voltage-level that last 50us,
-    the following high-voltage-level signal's length decide the bit is "1" or "0".
-    0: 26~28 us
-    1: 70 us
+
+The interval of whole process must be beyond 2 seconds.
+
+To request data from DHT:
+
+1) Sent low pulse for > 1~10 ms (MILI SEC)
+2) Sent high pulse for > 20~40 us (Micros).
+3) When DHT detects the start signal, it will pull low the bus 80us as response signal,
+   then the DHT pulls up 80us for preparation to send data.
+4) When DHT is sending data to MCU, every bit's transmission begin with low-voltage-level that last 50us,
+   the following high-voltage-level signal's length decide the bit is "1" or "0".
+  0: 26~28 us
+  1: 70 us
+
 ;----------------------------------------------------------------------------*/
 
 #define MAXdhtData 5 // to complete 40 = 5*8 Bits
@@ -141,25 +152,25 @@ int readDHT()
 
   // pull down for 3 ms for a smooth and nice wake up
   gpio_set_level(DHTgpio, 0);
-  ets_delay_us(3000);
+  esp_rom_delay_us(3000);
 
   // pull up for 25 us for a gentile asking for data
   gpio_set_level(DHTgpio, 1);
-  ets_delay_us(25);
+  esp_rom_delay_us(25);
 
   gpio_set_direction(DHTgpio, GPIO_MODE_INPUT); // change to input mode
 
   // == DHT will keep the line low for 80 us and then high for 80us ====
 
   uSec = getSignalLevel(85, 0);
-  //	ESP_LOGI( TAG, "Response = %d", uSec );
+  // ESP_LOGI(TAG, "Response = %d", uSec);
   if (uSec < 0)
     return DHT_TIMEOUT_ERROR;
 
   // -- 80us up ------------------------
 
   uSec = getSignalLevel(85, 1);
-  //	ESP_LOGI( TAG, "Response = %d", uSec );
+  // ESP_LOGI(TAG, "Response = %d", uSec);
   if (uSec < 0)
     return DHT_TIMEOUT_ERROR;
 
--- main/main.c
+++ main/main.c
@@ -7,29 +7,44 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 
+// -------- User Defined Library --------
 #include "DHT22.h"
 
-void DHT_reader_task(void *pvParameter)
+/*----------------------------------------------------
+
+Task:
+DHT Temperature and Humidity Reader
+from DHT22.h
+
+----------------------------------------------------*/
+void DHT_task(void *pvParameter)
 {
-  setDHTgpio(GPIO_NUM_34);
+  setDHTgpio(GPIO_NUM_32); // Set GPIO for DHT22 Sensor
 
   while (1)
   {
     printf("DHT Sensor Readings\n");
-    int ret = readDHT();
+    int ret = readDHT(); // Read Temperatuew and Humidity Value
 
-    errorHandler(ret);
+    errorHandler(ret); // checks for the response after reading from DHT22
 
-    printf("Humidity %.2f %%\n", getHumidity());
-    printf("Temperature %.2f degC\n\n", getTemperature());
+    printf("Humidity %.2f %%\n", getHumidity());           // get the humidity value
+    printf("Temperature %.2f degC\n\n", getTemperature()); // get the temperature value
 
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    vTaskDelay(2000 / portTICK_PERIOD_MS); // delay 2s
   }
 }
 
 void app_main()
 {
+  // initialize the NVS (Non-Volatile Storage)
   nvs_flash_init();
   vTaskDelay(2000 / portTICK_PERIOD_MS);
-  xTaskCreate(&DHT_reader_task, "DHT_reader_task", 2048, NULL, 5, NULL);
+
+  /*----------------------------------------------------
+
+  TASK CALLING
+
+  ----------------------------------------------------*/
+  xTaskCreate(&DHT_task, "DHT_task", 2048, NULL, 5, NULL);
 }
