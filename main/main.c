ea1771b87795f6cfe85f13542e0be42628346543
Naufal Muafi <nmuafi1@gmail.com>
2023-10-09 21:49:47 +0700

    add: object kernel

--- main/main.c
+++ main/main.c
@@ -15,6 +15,9 @@
 #include "esp_adc_cal.h"
 #include "driver/i2c.h"
 #include "esp_err.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+#include "driver/dac.h"
 
 // -------- User Defined Library --------
 #include "DHT22.h"
@@ -38,11 +41,41 @@
 #define LDR_PIN ADC1_CHANNEL_6 // GPIO 34
 
 // Motor Servo Declaration
+#define SERVO_PIN GPIO_NUM_25
+#define LEDC_TIMER LEDC_TIMER_0
+#define LEDC_MODE LEDC_HIGH_SPEED_MODE
+#define LEDC_CHANNEL LEDC_CHANNEL_0
+#define LEDC_DUTY_RESOLUTION LEDC_TIMER_13_BIT
 
-
-// Object Declaration
+// -------- OBJECT DECLARATION --------
+// ADC Declaration
 static esp_adc_cal_characteristics_t CHANNEL_ADC;
+
+// OLED Declaration
 SSD1306_t dev;
+
+// Queue Declaration: Send Data Potentiometer
+QueueHandle_t pot_queue;
+int           pot_receive;
+
+// Queue Declaration: Send Data Photoresistor
+QueueHandle_t ldr_queue;
+int           ldr_receive;
+
+// Mutex Declaration: Create 1 Mutex for Critical Session
+SemaphoreHandle_t mutex;
+
+
+/*----------------------------------------------------
+
+Function:
+Mapping Value/Normalization
+
+----------------------------------------------------*/
+long map(long x, long i_min, long i_max, long o_min, long o_max)
+{
+  return (x - i_min)*(o_max - o_min) / (i_max - i_min) + o_min;
+}
 
 
 /*----------------------------------------------------
@@ -76,12 +109,6 @@ from DHT22.h
 void DHT_task(void *pvParameter)
 {
   setDHTgpio(DHT22_PIN); // Set GPIO for DHT22 Sensor
-
-  SSD1306_t dev;
-  i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
-  ssd1306_init(&dev, 128, 32);
-  ssd1306_clear_screen(&dev, false);
-  ssd1306_contrast(&dev, 0xff);
 
   while (1)
   {
@@ -128,15 +155,10 @@ void potentiometer_task(void *pvParameter)
 
   while (1)
   {
-    int adc_value = adc1_get_raw(POT_PIN); // capture RAW ADC Value
-    // convert to mV    
-    uint32_t mV = esp_adc_cal_raw_to_voltage(adc1_get_raw(POT_PIN), &CHANNEL_ADC);
+    int adc_value = adc1_get_raw(POT_PIN); // capture RAW ADC Value    
 
-    // display value on serial terminal
-    printf("ADC Value: %d\n", adc_value);
-    printf("Nilai Potentiometer: %ld mV\n\n", mV);
-
-    vTaskDelay(pdMS_TO_TICKS(2000)); // delay for 0.5s
+    xQueueSend(pot_queue, &adc_value, (TickType_t)0);
+    vTaskDelay(10 / portTICK_PERIOD_MS); // delay for 10 ms
   }
 }
 
@@ -160,10 +182,63 @@ void ldr_task(void *pvParameter)
   {
     int adc_value = adc1_get_raw(LDR_PIN); // capture RAW ADC Value from LDR Sensor
 
-    // display value on serial terminal
-    printf("LDR ADC Value: %d\n\n", adc_value);    
+    xQueueSend(ldr_queue, &adc_value, (TickType_t)0);
 
-    vTaskDelay(pdMS_TO_TICKS(2000)); // delay for 0.5s
+    vTaskDelay(10 / portTICK_PERIOD_MS); // delay for 10 ms
+  }
+}
+
+/*----------------------------------------------------
+
+Task:
+Motor Servo Motion
+
+----------------------------------------------------*/
+void servo_task(void *pvParameter)
+{
+  // Configure GPIO for servo control
+  gpio_config_t io_conf = {
+      .mode = GPIO_MODE_OUTPUT,
+      .pin_bit_mask = (1ULL << SERVO_PIN),
+      .intr_type = GPIO_INTR_DISABLE,
+      .pull_down_en = 0,
+      .pull_up_en = 0,
+  };
+  gpio_config(&io_conf);
+
+  // Configure LEDC for PWM control
+  ledc_timer_config_t timer_conf = {
+      .duty_resolution = LEDC_DUTY_RESOLUTION,
+      .freq_hz = 50,
+      .speed_mode = LEDC_MODE,
+      .timer_num = LEDC_TIMER,
+      .clk_cfg = LEDC_AUTO_CLK,
+  };
+  ledc_timer_config(&timer_conf);
+
+  ledc_channel_config_t channel_conf = {
+      .channel = LEDC_CHANNEL,
+      .duty = 0,
+      .gpio_num = SERVO_PIN,
+      .speed_mode = LEDC_MODE,
+      .timer_sel = LEDC_TIMER,
+      .hpoint = 0,
+  };
+  ledc_channel_config(&channel_conf);
+
+  while(1)
+  {
+    if (xSemaphoreTake(mutex, portMAX_DELAY) == pdTRUE)
+    {
+      xQueueReceive(pot_queue, &pot_receive, (TickType_t)5);
+      int val = map(pot_receive, 0, 4095, 200, 1000);
+
+      ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, val);
+      ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+
+      xSemaphoreGive(mutex);
+    }
+    vTaskDelay(10/portTICK_PERIOD_MS); // delay for 10 ms
   }
 }
 
@@ -181,10 +256,21 @@ void app_main()
 
   /*----------------------------------------------------
 
+  OBJECT METHOD DECLARATION
+
+  ----------------------------------------------------*/
+  pot_queue = xQueueCreate(5, sizeof(int));
+  ldr_queue = xQueueCreate(5, sizeof(int));
+
+  mutex = xSemaphoreCreateMutex();
+
+  /*----------------------------------------------------
+
   TASK CALLING
 
   ----------------------------------------------------*/
   xTaskCreate(&DHT_task, "DHT_task", 2048*2, NULL, 5, NULL);
-  xTaskCreate(potentiometer_task, "potentiometer_task", 2048*2, NULL, 5, NULL);
-  xTaskCreate(ldr_task, "ldr_task", 2048*2, NULL, 5, NULL);
+  xTaskCreate(&potentiometer_task, "potentiometer_task", 2048*2, NULL, 4, NULL);
+  xTaskCreate(ldr_task, "ldr_task", 2048*2, NULL, 4, NULL);
+  xTaskCreate(servo_task, "servo task", 2048*2, NULL, 4, NULL);  
 }
